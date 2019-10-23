9c6fe9d5919779efd7a96186d9d83e79da7c3cad
Setorator <setorator@gmail.com>
2019-10-23 14:40:30 +0200

    Fixed dynamic file-paths to the map's img files

--- res/maps/tileset.tsx
+++ res/maps/tileset.tsx
@@ -2,27 +2,27 @@
 <tileset version="1.2" tiledversion="1.2.5" name="tileset" tilewidth="590" tileheight="336" tilecount="8" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="1">
-  <image width="64" height="64" source="C:/Users/kaskling/PycharmProjects/zombiemania/zombiemania/res/img/grassblock.png"/>
+  <image width="64" height="64" source="../img/grassblock.png"/>
  </tile>
  <tile id="2">
-  <image width="64" height="64" source="C:/Users/kaskling/PycharmProjects/zombiemania/zombiemania/res/img/dirtblock.png"/>
+  <image width="64" height="64" source="../img/dirtblock.png"/>
  </tile>
  <tile id="3">
-  <image width="64" height="64" source="C:/Users/kaskling/PycharmProjects/zombiemania/zombiemania/res/img/woodblock.png"/>
+  <image width="64" height="64" source="../img/woodblock.png"/>
  </tile>
  <tile id="4">
-  <image width="64" height="64" source="C:/Users/kaskling/PycharmProjects/zombiemania/zombiemania/res/img/spikes.png"/>
+  <image width="64" height="64" source="../img/spikes.png"/>
  </tile>
  <tile id="5">
-  <image width="100" height="60" source="C:/Users/kaskling/PycharmProjects/zombiemania/zombiemania/res/img/instructions.png"/>
+  <image width="100" height="60" source="../img/instructions.png"/>
  </tile>
  <tile id="6">
-  <image width="590" height="336" source="C:/Users/kaskling/PycharmProjects/zombiemania/zombiemania/res/img/background.gif"/>
+  <image width="590" height="336" source="../img/background.gif"/>
  </tile>
  <tile id="9">
-  <image width="100" height="100" source="C:/Users/kaskling/PycharmProjects/zombiemania/zombiemania/res/img/start.png"/>
+  <image width="100" height="100" source="../img/start.png"/>
  </tile>
  <tile id="10">
-  <image width="32" height="64" source="C:/Users/kaskling/PycharmProjects/zombiemania/zombiemania/res/img/gateway.gif"/>
+  <image width="32" height="64" source="../img/gateway.gif"/>
  </tile>
 </tileset>
